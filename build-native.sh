ad50950a6a65793617e5051bc33a93b2a9fb7658
zhonggao.wang <zhonggao.wang@yitu-inc.com>
2021-04-01 16:02:12 +0800

    struggle with graal

--- build-native.sh
+++ build-native.sh
@@ -1,3 +1,3 @@
 ./gradlew --no-daemon assemble
 sdk use java 20.3.0.r11-grl
-native-image --no-server -cp build/libs/todo-cli-*-all.jar
+native-image --initialize-at-build-time=java.sql.DriverManager --no-server -cp build/libs/todo-cli-*-all.jar
